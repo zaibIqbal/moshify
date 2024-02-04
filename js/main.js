f64cc80b9613a0ecd9f72066326b4bb9d0738c00
Shahzaib Iqbal <ishahzaib07@gmail.com>
2024-02-03 22:40:12 -0800

    Fix the bug with collapsibles

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
-const collapsibles = document.querySelectorAll(".collapsible");
+let collapsibles = document.getElementsByClassName("collapsible");
+console.log(collapsibles);
+collapsibles = Array.from(collapsibles);
 collapsibles.forEach((item) =>
   item.addEventListener("click", function () {
     this.classList.toggle("collapsible--expanded");
   })
 );
+
+console.log(collapsibles);
